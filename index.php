a3942340d199313c4519d5f3cac1a0fb9f4b2d59
Quill McConnell <quill8197@yahoo.com>
2019-04-03 17:26:29 -0400

    Added a function that prints each item in an array on a new line. Added a new numbers array to test the function.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  * Time: 2:13 PM
  */
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,5 +18,19 @@
 </head>
 <body>
     <h1>Pair Program 1</h1>
+    <?php
+    //a function that prints each item in an array
+    function printArr($items)
+    {
+        foreach ($items as $item)
+        {
+            echo $item . "<br>";
+        }
+    }
+
+    //create an array and print it
+    $numbers = array(7, 9, 8, 9, 8, 8, 6);
+    printArr($numbers);
+    ?>
 </body>
 </html>
